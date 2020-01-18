404966743b7a4064ec0aa56fe1022cba80fe1d70
G <54356938+BG4Finance@users.noreply.github.com>
2020-01-18 17:51:04 +0100

    Graph creation

--- GDP% vs. Consumption/GDPvsConsumption.R
+++ GDP% vs. Consumption/GDPvsConsumption.R
@@ -30,12 +30,14 @@ ggplot(data12, aes(x=GDPpc, y=consumptionKW, size=GDP, color=regions, label = re
   geom_point(alpha=0.5) +
   scale_size(range = c(1, 50), name="GDP") +
   geom_text(size=6,hjust=0, vjust=0) +
-  expand_limits(x = c(0, 5)) +
-  labs(title = "GDP vs. Electric Consumption pro capita") +
+  expand_limits(x = c(0, 6), y = c(0,17000)) +
+  labs(title = "GDP vs. Electric Consumption pro capita, Width is actual GDP pro capita ") +
   labs(x = "GDP growth % pro capita", y = "Electric consumption pro capita (KW)") +
   labs(caption = "(based on data from World Bank Dataset)") +
-  theme(legend.key.size = unit(0.05, "cm"))
-  
+  theme(legend.key.size = unit(0.05, "cm"), panel.background = 
+          element_rect(fill = "white", colour = "#6D9EC1"),
+        panel.grid.major = element_line(size = 0.25, linetype = 'solid', colour = "gray")
+        )
 
 
   
