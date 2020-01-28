5ca0e84b2c2cdf0e32a33bd49ad0371595a93e7e
G <54356938+BG4Finance@users.noreply.github.com>
2020-01-28 23:15:02 +0100

    Update WC

--- WordCloud/wordcloud.py
+++ WordCloud/wordcloud.py
@@ -1,20 +1,22 @@
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import pandas as pd
 
-#Modify hereby the path to text.csv
-df = pd.read_csv("/WordCloud/text.csv", header=None)
+#Setup words
+df = pd.read_csv("/content/text.csv", header=None)
 df = df.to_numpy()
 
 text = df[0]
 for i in range(1,df.shape[0]):
-  text += ' '
+  text += ', '
   text += df[i]
 
 text = str(text)
-wordcloud = WordCloud().generate(text)
+wordcloud = WordCloud(background_color="white", stopwords=STOPWORDS, random_state=63).generate(text)
+
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
